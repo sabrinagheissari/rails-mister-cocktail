ac608f1ac26e9956b340fb3eb46d5bc237d49eee
Sabrina Gheissari <sabrina.gheissari1@gmail.com>
2019-11-07 13:53:54 +0900

    done

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,25 @@
+class CocktailsController < ApplicationController
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+    redirect_to cocktail_path
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+  end
+end
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,3 +1,5 @@
 class Cocktail < ApplicationRecord
-  has_many :doses, presence: true
+  has_many :doses, dependent: :destroy
+  validates :name, presence: true, uniqueness: true
+  has_many :ingredients, through: :doses
 end
